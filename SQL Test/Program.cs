bddd7bf236ea47dfc789ca0c684ce6d323227309
Igor <75262127+Mazepin@users.noreply.github.com>
2021-03-07 16:01:40 +0300

    feat: add context songs

--- SQL Test/Program.cs
+++ SQL Test/Program.cs
@@ -10,19 +10,50 @@ namespace SQL_Test
     {
         static void Main(string[] args)
         {
-            
-            using(var context = new MyDbContext())
+
+            using (var context = new MyDbContext())
             {
+
                 var group = new Group()
                 {
                     Name = "Rammstain",
                     Year = 1994
                 };
 
+                var group2 = new Group()
+                {
+                    Name = "Linkin Park",
+                };
+
+                var group3 = new Group()
+                {
+                    Name = "Prodigy",
+                    Year = 1990
+                };
+
+
                 context.Groups.Add(group); // добавляем группу в контекст, но она еще не в базе данных. Нужно еще сохранить
+                context.Groups.Add(group2);
+                context.Groups.Add(group3);
                 context.SaveChanges(); // сохраняем все изменения из локального хранилища в базу данных
 
-                Console.WriteLine($"id: {group.Id}, name: {group.Name}, year: {group.Year}");
+
+                var songs = new List<Song>()
+                {
+                    new Song() {Name = "In The End", GroupId = group2.Id },
+                    new Song() {Name = "Numb", GroupId = group2.Id},
+                    new Song() {Name = "Mutter", GroupId = group.Id},
+                    new Song() {Name = "Deutschland", GroupId = group.Id},
+                    new Song() {Name = "Smack My Bits Up", GroupId = group3.Id}
+                };
+
+                context.Songs.AddRange(songs); // добавляем сразу коллекцию
+                context.SaveChanges();
+
+                foreach (var song in songs)
+                {
+                    Console.WriteLine($"Song name: {song.Name}, \tGroup name: {song.Group?.Name}");
+                }
                 Console.ReadKey();
             }
         }
